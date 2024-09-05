7846a1f52ac3be9da5d20788db49a8dfaad388d3
Jiya P <jiyapalod@gmail.com>
2024-09-05 11:33:17 +0530

    Create constructor_outside.cpp

---
+++ constructor_outside.cpp
@@ -0,0 +1,50 @@
+#include<iostream>
+using namespace std;
+class Student
+{
+    int roll_no;
+    char name[50];
+    float fee;
+
+    public: 
+    Student();
+    void display();
+};
+
+Student::Student()
+{
+    cout<<"Enter name:"<<endl;
+    cin>>name;
+    cout<<"Enter roll number:"<<endl;
+    cin>>roll_no;
+    cout<<"Enter fees:"<<endl;
+    cin>>fee;
+}
+
+void Student::display()
+{
+    cout<<"\nDisplaying Data:"<<endl;
+    cout<<roll_no<<endl<<name<<endl<<fee<<endl;
+}
+
+int main()
+{
+    Student s1;
+    s1.display();
+    return 0;
+}
+
+/*
+OUTPUT: 
+Enter name:
+Jiya
+Enter roll number:
+70
+Enter fees:
+3500
+
+Displaying Data:
+70
+Jiya
+3500
+*/
