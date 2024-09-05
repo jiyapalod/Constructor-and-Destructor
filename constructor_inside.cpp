d3ac2ea60d52029b067e905b53112da5f0237ede
Jiya P <jiyapalod@gmail.com>
2024-09-05 11:20:24 +0530

    Create constructor_inside.cpp

---
+++ constructor_inside.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+class Date
+{
+    int d, m, y;
+    public: 
+    Date()
+    {
+        cout<<"Today's Date is: "<<endl;
+        d=5; 
+        m=9;
+        y=2024;
+        cout<<d<<"/"<<m<<"/"<<y;
+    }
+    
+};
+int main()
+{
+    Date today;
+}
+
+/*
+OUTPUT: 
+Today's Date is: 
+5/9/2024
+*/
